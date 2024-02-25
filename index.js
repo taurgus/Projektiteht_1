b04ba8b4a8612efae441c264cc2a0118fd6c7c71
taurgus <100350635+taurgus@users.noreply.github.com>
2024-02-25 15:30:42 +0200

    port 3005

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Route: /
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Questbookki');
 });
 
 // Route: /guestbook
