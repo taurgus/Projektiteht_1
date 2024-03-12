a6ec3e9a19caf3be890224e0f50211dcbb64217f
taurgus <100350635+taurgus@users.noreply.github.com>
2024-03-12 09:49:00 +0200

    json

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
+//Dependecies
 const express = require('express');
 const app = express();
 const port = 3005;
 
-// Route: / 
+// Juurireitti
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Route: /guestbook
-app.get('/guestbook', (req, res) => {
-  res.send('Welcome to the guestbook!');
+
+//Parse .json into a table
+app.get('/guestbook', function (req, res) {
+  var data = require(__dirname + '/data/data.json');
+
+  var results = '<table border="1"> ';
+
+  for (var i = 0; i < data.length; i++) {
+      results +=
+          '<tr>' +
+          '<td>' + data[i].id + '</td>' +
+          '<td>' + data[i].username + '</td>' +
+          '<td>' + data[i].country + '</td>' +
+          '<td>' + data[i].date + '</td>' +
+          '<td>' + data[i].message + '</td>' +
+          '</tr>';
+  }
+
+  res.send(results);
 });
+
+
+
 
 // Route: /newmessage
 app.get('/newmessage', (req, res) => {
   res.send('Create a new message');
+
 });
 
-// Route: /ajaxmessage
-app.get('/ajaxmessage', (req, res) => {
-  res.send('Handle AJAX request for messages');
-});
 
 app.get('*', function (req, res) {
-  res.send('Cant find the requested page', 404);
+  res.send('Sivua ei löydy', 404);
 });
 
 app.listen(port, () => {
